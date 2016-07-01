85ffdc31d6f2a679d8fe02b3c2204be42adaa45f
CharlesAMoss <charlesamoss@yahoo.com>
2016-07-01 16:30:23 -0700

    initial commit

---
+++ src/Main.hs
@@ -0,0 +1,9 @@
+module Main where
+
+main :: IO ()
+main = do
+  putStrLn "HAMURABI"
+  putStrLn "CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY"
+  putStrLn " "
+  putStrLn "TRY YOUR HAND AT GOVERNING ANCIENT SUMERIA"
+  putStrLn "FOR A TEN-YEAR TERM OF OFFICE."
